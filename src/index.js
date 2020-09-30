03bcd6fc0ee623299aae318e27699afcc4ba6647
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-10-01 00:02:05 +0500

    add base webpack and base babel MiniCssExtractPlugin

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+console.log('Hello, Webpack!')
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+
+module.exports = {
+  entry: {main: './src/index.js'},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: {loader: "babel-loader"},
+      exclude: /node_modules/
+    },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: 'style.css'})
+  ]
+}
