3b87a2460108b1ea28e4588ee07595b4b601c6d2
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-10-04 17:27:24 +0500

    add webpack and babel options

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const webpack = require("webpack");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 
 const path = require('path');
 
@@ -6,21 +11,71 @@ module.exports = {
   entry: {main: './src/index.js'},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [{
       test: /\.js$/,
-      use: {loader: "babel-loader"},
-      exclude: /node_modules/
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-syntax-dynamic-import",
+              "@babel/plugin-proposal-class-properties",
+            ],
+          },
+        },
+      ],
+      exclude: /node_modules/,
     },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
+        test: /\.css$/i,
+        use: [
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg|jpeg)$/,
+        use: [
+          "file-loader?name=./images/[name].[ext]&esModule=false",
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)$/,
+        use: "file-loader?name=./fonts/[name].[ext]",
+      },
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({filename: 'style.css'})
+    new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default"],
+      },
+      canPrint: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ]
 }
