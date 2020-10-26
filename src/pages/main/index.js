5a831b2cf921a7bb3103176d1c4a2b3ec062ab66
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-10-26 16:59:59 +0500

    refactor code style in index.js

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -17,31 +17,31 @@ import mobileMenuClose from "../../script/utils/mobile-menu-close";
   const mainApi = new MainApi(MAIN_API_OPTIONS);
 
   const pageElement = document.querySelector('.page');
-  const signUpButton = document.querySelector('#signup-button');
-  const mobileOpenButton = document.querySelector('.header__menu');
-  const mobileCloseButton = document.querySelector('.header__menu-close');
-  const headerNavigation = document.querySelector('.header__navigation');
   const popupSignContainer = document.querySelector('.popup_type_signup');
   const popupRegisteredContainer = document.querySelector('.popup_type_registered');
   const popupSignInContainer = document.querySelector('.popup_type_signin');
+  const headerContainer = document.querySelector('.header');
+  const headerNavigation = document.querySelector('.header__navigation');
+  const newsContainer = document.querySelector('.news');
+  const signForm = document.forms.signup;
+  const signInForm = document.forms.signin;
+  const searchContainer = document.forms.searchForm;
+  const signUpButton = document.querySelector('#signup-button');
+  const mobileOpenButton = document.querySelector('.header__menu');
+  const mobileCloseButton = document.querySelector('.header__menu-close');
   const signInPopupCloseButton = popupSignInContainer.querySelector('.popup__close');
   const signUpPopupCloseButton = popupSignContainer.querySelector('.popup__close');
   const redirectSignInButton = popupSignInContainer.querySelector('.popup__link');
   const redirectSignUpButton = popupSignContainer.querySelector('.popup__link');
   const redirectRegisteredButton = popupRegisteredContainer.querySelector('.popup__signup-link');
   const registeredPopupCloseButton = popupRegisteredContainer.querySelector('.popup__close');
-  const signForm = document.forms.signup;
-  const signInForm = document.forms.signin;
-  const searchContainer = document.forms.searchForm;
   const signUpSubmitButton = signForm.querySelector('.button');
   const signInSubmitButton = signInForm.querySelector('.button');
   const searchSubmitButton = searchContainer.querySelector('.search-form__button')
-  const headerContainer = document.querySelector('.header');
   const logoutButton = headerContainer.querySelector('.header__button-text');
   const preloader = document.querySelector('.preloader');
   const notFound = document.querySelector('.not-found');
-  const newsContainer = document.querySelector('.news');
-  const showMoreButton = document.querySelector('.news__button')
+  const showMoreButton = document.querySelector('.news__button');
 
   const header = new Header(logoutButton, headerContainer, mainApi, MAIN_PATH);
   const card = new NewsCard(newsApi, mainApi);
@@ -50,18 +50,14 @@ import mobileMenuClose from "../../script/utils/mobile-menu-close";
 
   const popupSignIn = new Popup(signInPopupCloseButton, popupSignInContainer, pageElement);
   const popupSignUp = new Popup(signUpPopupCloseButton, popupSignContainer, pageElement);
-// popupSignUp.setEventListeners();
 
   const popupRegistered = new Popup(registeredPopupCloseButton, popupRegisteredContainer, pageElement);
-// popupRegistered.setEventListeners()
 
   const signUpForm = new Form(signUpSubmitButton, signForm, mainApi, popupSignUp, popupRegistered);
   signUpForm.setEventListeners();
 
   const loginForm = new Form(signInSubmitButton, signInForm, mainApi, popupSignIn, popupRegistered);
   loginForm.setEventListeners()
-// signUpForm.getButtons()
-// popupSignUp.open()
 
   const searchForm = new SearchForm(searchSubmitButton, searchContainer, newsApi, notFound, cardList);
   searchForm.setEventListeners();
@@ -70,19 +66,6 @@ import mobileMenuClose from "../../script/utils/mobile-menu-close";
     currentPopup.close();
     newPopup.open();
   }
-
-  // const mobileMenuOpen = function (openButton, closeButton, nav, page) {
-  //   openButton.classList.add('header__menu_hidden')
-  //   closeButton.classList.remove('header__menu-close_hidden')
-  //   nav.classList.add('header__navigation_mobile-opened')
-  //   page.classList.add('page_scroll-off')
-  // }
-  // const mobileMenuClose = function (openButton, closeButton, nav, page) {
-  //   closeButton.classList.add('header__menu-close_hidden')
-  //   openButton.classList.remove('header__menu_hidden')
-  //   nav.classList.remove('header__navigation_mobile-opened')
-  //   page.classList.remove('page_scroll-off')
-  // }
 
   mobileOpenButton.addEventListener('click', () => mobileMenuOpen(mobileOpenButton, mobileCloseButton, headerNavigation, pageElement));
   mobileCloseButton.addEventListener('click', () => mobileMenuClose(mobileOpenButton, mobileCloseButton, headerNavigation, pageElement));
